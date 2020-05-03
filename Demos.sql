aad7c20c43476eeca7e014886f96239152d9aa38
JasonLee197 <qinghan_Lee@163.com>
2020-05-03 13:31:40 -0400

    create database

---
+++ Demos.sql
@@ -0,0 +1,42 @@
+
+--Test the procedure of change status by admins
+SELECT * 
+FROM Candidates
+WHERE candidateId = 2;
+
+EXEC spChangeStatus 'Nick', 'Snow', 5;
+
+SELECT * 
+FROM Candidates
+WHERE candidateId = 2;
+
+
+
+-- test the function of calculate the total cost of Reimbursement
+UPDATE Reimbursement SET cost = dbo.fnCaculateTotalCost(1) WHERE reimburseId = 1;
+
+select * from Reimbursement where reimburseId = 1;
+
+
+PRINT '$ ' + CAST(dbo.fnCaculateTotalCost(1) AS VARCHAR);
+
+
+
+-- test the procedure of set up a new apply
+select * from Candidates WHERE candidateId = 5;
+
+EXEC spNewApply 'Coulson', 'Stark';
+
+select * from Candidates WHERE candidateId = 5;
+
+
+
+-- test trigger
+select OpeningNumber from JobOpenings;
+select statusId from Candidates where candidateId = 2 or candidateId = 6;
+-- extend offer
+EXEC spChangeStatus 'Nick', 'Snow', 5;
+-- someone accept the offer
+EXEC spChangeStatus 'Daisy', 'Skye', 7;
+select OpeningNumber from JobOpenings;
+select statusId from Candidates where candidateId = 2 or candidateId = 6;
